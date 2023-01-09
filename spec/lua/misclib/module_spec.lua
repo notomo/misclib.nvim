9bd00fe820fcaafbe329acc628d7b8362b12f327
notomo <notomo.motono@gmail.com>
2023-01-09 12:11:20 +0900

    Small refactor test

--- spec/lua/misclib/module_spec.lua
+++ spec/lua/misclib/module_spec.lua
@@ -6,9 +6,8 @@ describe("modulelib.find()", function()
   after_each(helper.after_each)
 
   it("returns the module if found", function()
-    helper.test_data:create_dir("lua")
-    vim.opt.runtimepath:prepend(helper.test_data.full_path)
     helper.test_data:create_file("lua/valid.lua", [[return {ok = 8888}]])
+    vim.opt.runtimepath:prepend(helper.test_data.full_path)
 
     local m = modulelib.find("valid")
     assert.is_same({ ok = 8888 }, m)
@@ -20,9 +19,8 @@ describe("modulelib.find()", function()
   end)
 
   it("raises error if found but error", function()
-    helper.test_data:create_dir("lua")
-    vim.opt.runtimepath:prepend(helper.test_data.full_path)
     helper.test_data:create_file("lua/error.lua", [[error("raised error", 0)]])
+    vim.opt.runtimepath:prepend(helper.test_data.full_path)
 
     local ok, result = pcall(modulelib.find, "error")
     assert.is_false(ok)
