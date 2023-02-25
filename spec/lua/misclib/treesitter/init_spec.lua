08e0200c487a2a022845f959f520f5e14dd288d6
notomo <notomo.motono@gmail.com>
2023-02-25 15:08:27 +0900

    Add treesitter lib

---
+++ spec/lua/misclib/treesitter/init_spec.lua
@@ -0,0 +1,17 @@
+local helper = require("misclib.test.helper")
+local tslib = helper.require("misclib.treesitter")
+
+describe("tslib.has_parser()", function()
+  before_each(helper.before_each)
+  after_each(helper.after_each)
+
+  it("returns true if parser exists", function()
+    local got = tslib.has_parser("lua")
+    assert.is_true(got)
+  end)
+
+  it("returns false if parser does not exist", function()
+    local got = tslib.has_parser("not_found")
+    assert.is_false(got)
+  end)
+end)
