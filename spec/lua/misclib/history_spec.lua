433f1083750e2cce5ebc22ed105cb16868f3c746
notomo <notomo.motono@gmail.com>
2023-04-22 18:04:13 +0900

    Add historylib

---
+++ spec/lua/misclib/history_spec.lua
@@ -0,0 +1,82 @@
+local helper = require("misclib.test.helper")
+local historylib = helper.require("misclib.history")
+
+describe("historylib", function()
+  before_each(function()
+    helper.before_each()
+    vim.fn.histdel("input")
+  end)
+  after_each(helper.after_each)
+
+  it("recalls does nothing if there is no stored", function()
+    local store = historylib.new("input")
+    local got = store:recall(-1)
+    assert.is_nil(got)
+  end)
+
+  it("can recalls backward", function()
+    local store = historylib.new("input")
+    store:save("1")
+    store:save("2")
+
+    do
+      local got = store:recall(-1)
+      assert.equal("2", got)
+    end
+
+    do
+      local got = store:recall(-1)
+      assert.equal("1", got)
+    end
+  end)
+
+  it("can recalls forward", function()
+    local store = historylib.new("input")
+    store:save("1")
+    store:save("2")
+
+    do
+      store:recall(-1)
+      local got = store:recall(-1)
+      assert.equal("1", got)
+    end
+
+    do
+      local got = store:recall(1)
+      assert.equal("2", got)
+    end
+  end)
+
+  it("can recalls before", function()
+    local store = historylib.new("input")
+    store:save("1")
+    store:save("2")
+
+    do
+      local got = store:recall(-1, "3")
+      assert.equal("2", got)
+    end
+
+    do
+      local got = store:recall(1)
+      assert.equal("3", got)
+    end
+  end)
+
+  it("can recalls with filter", function()
+    vim.fn.histadd("input", "1")
+    vim.fn.histadd("input", "ignored1")
+
+    local store = historylib.new("input", {
+      filter = function(history)
+        return #history == 1
+      end,
+    })
+    store:save("ignored2")
+
+    do
+      local got = store:recall(-1)
+      assert.equal("1", got)
+    end
+  end)
+end)
