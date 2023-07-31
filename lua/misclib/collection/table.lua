49d9853021b66bbb69a6462a0d7664bd2badf14c
notomo <notomo.motono@gmail.com>
2023-07-31 09:13:22 +0900

    Use vim.iter instead of continue

--- lua/misclib/collection/table.lua
+++ lua/misclib/collection/table.lua
@@ -2,18 +2,16 @@ local M = {}
 
 function M.group_by(list, make_key)
   local groups = {}
-  for _, element in ipairs(list) do
+  vim.iter(list):each(function(element)
     local key = make_key(element)
     if not key then
-      goto continue
+      return
     end
 
     local elements = groups[key] or {}
     table.insert(elements, element)
     groups[key] = elements
-
-    ::continue::
-  end
+  end)
   return groups
 end
 
